1ac297ce458d30c8872002fafd316c2fa138075d
yishuangxi <2546618112@qq.com>
2016-09-15 22:43:09 +0800

    interface.go

--- go-rookie/018-interface.go
+++ go-rookie/018-interface.go
@@ -5,6 +5,33 @@ import (
 )
 
 func main(){
+	var phone1 Phone
+	var phone2 Phone
+	phone1 = new(NokiaPhone)
+	phone2 = new(IPhone)
+
+	phone1.call()
+	phone2.call()
+}
+
+type Phone interface {
+	call()
+}
+
+type IPhone struct {
 
 }
+
+type NokiaPhone struct {
+}
+
+func (iPhone *IPhone) call(){
+	fmt.Println("iphone calling")
+}
+
+func (nokiaPhone *NokiaPhone) call(){
+	fmt.Println("nokiaphone calling")
+}
+
+
 
